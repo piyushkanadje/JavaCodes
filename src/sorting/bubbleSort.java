497a31bf2dcc28fefc927b8aeb84aa681d4f8f09
KARMASPHERE <piyushkanadje@gmail.com>
2021-06-20 17:16:19 +0530

    20/6

--- src/sorting/bubbleSort.java
+++ src/sorting/bubbleSort.java
@@ -68,6 +68,25 @@ public class bubbleSort {
 		}
 	}
 
+	static void Bubble(int[] a) {
+		int n = a.length;
+		for (int i = 0; i < n; i++) {
+			boolean swapped = true;
+			for (int j = 0; j < n - 1 - i; j++) {
+				if (a[j] > a[j + 1]) {
+					int temp = a[j];
+					a[j] = a[j + 1];
+					a[j + 1] = temp;
+					swapped = true;
+				}
+				if (swapped) {
+					break;
+				}
+
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a[] = { 2, 1, 4, 3 };
