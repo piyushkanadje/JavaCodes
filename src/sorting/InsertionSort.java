4bb42ec57b464d0419fc774961a5df56cfbfdb7c
KARMASPHERE <piyushkanadje@gmail.com>
2021-06-28 21:19:45 +0530

    28/6

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -25,7 +25,20 @@ public class InsertionSort {
 				j--;
 			}
 			a[j + 1] = key;
-			
+
+		}
+	}
+
+	static void insertionSortss(int[] a) {
+		int n = a.length;
+		for (int i = 0; i < n; i++) {
+			int key = a[i];
+			int j = i - 1;
+			while (j >= 0 && a[j] > key) {
+				a[j + 1] = a[j];
+				j--;
+			}
+			a[j + 1] = key;
 		}
 	}
 
