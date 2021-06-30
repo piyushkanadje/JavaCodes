5d74dd292af1255a1dcfa0801a4155a05b3e53e3
KARMASPHERE <piyushkanadje@gmail.com>
2021-06-30 23:09:49 +0530

    '30/6

--- src/com/semanticsquare/opp/codes/Divisor.java
+++ src/com/semanticsquare/opp/codes/Divisor.java
@@ -22,24 +22,33 @@ public class Divisor {
 			}
 		}
 	}
-	
-	static void a (int n) {
-		for(int i = 1;  i *i<= n ; i++) {
-			if(n%i == 0) {
+
+	static void a(int n) {
+		for (int i = 1; i * i <= n; i++) {
+			if (n % i == 0) {
 				System.out.println(i);
-				if(i!=n/i) {
-					System.out.println(n/i);
+				if (i != n / i) {
+					System.out.println(n / i);
 				}
 			}
 		}
 	}
 
-	
+	static void div(int n) {
+		for (int i = 1; i * i < n; i++) {
+			if (n % i == 0) {
+				System.out.println(i);
+				if (i != n / i) {
+					System.out.println(n / i);
+				}
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		a(20);
-	
+		div(20);
+
 		// printDivisors(20);
 		// System.out.println(divisorO(25));
 		// System.out.println(divisorrs(12));
